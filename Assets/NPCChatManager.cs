eb951e0958579ffa45a853bfc39f6484d32c3ef7
Edgar <edirc98@gmail.com>
2024-03-18 11:22:42 +0100

    NPC now responds given a personality and a scene description

--- Assets/NPCChatManager.cs
+++ Assets/NPCChatManager.cs
@@ -5,6 +5,16 @@ using OpenAI;
 
 public class NPCChatManager : MonoBehaviour
 {
+
+    #region NPC TRAITS
+    [Header("NPC CONFIGURATION")]
+    [TextArea(5, 20)]
+    public string NPC_Personality;
+    [TextArea(5, 20)]
+    public string NPC_Scene;
+    public int MaxResponseTokens = 32; 
+    #endregion
+
     #region PARAMETERS & VARIABLES
     public string TestMessage = null; 
     //OpenAI Api constructor
@@ -13,31 +23,38 @@ public class NPCChatManager : MonoBehaviour
     //List of request messages
     private List<ChatMessage> messages = new List<ChatMessage>();
     #endregion
+
     #region START
     void Start()
     {
         MakeRequest(TestMessage); 
     }
     #endregion
+
     #region UPDATE
     void Update()
     {
         
     }
     #endregion
+
     #region OPEN AI API FUNCTIONS
-    public async void MakeRequest(string newText)
+    public async void MakeRequest(string userMessageText)
     {
         //Create message 
-        ChatMessage newMessage =  CreateChatMessage("user", newText);
+        ChatMessage newMessage =  CreateChatMessage("user", userMessageText);
         Debug.Log("ROLE: " + newMessage.Role);
         Debug.Log("MESSAGE:" + newMessage.Content);
+
+        //Check if this is the first message, if true, add the instructions before the message
+        if (messages.Count == 0) newMessage.Content = SetNPCInstructions() + "\n" + userMessageText;
         messages.Add(newMessage);
 
         //Create Request 
         CreateChatCompletionRequest request = new CreateChatCompletionRequest();
         request.Model = "gpt-3.5-turbo"; 
         request.Messages = messages;
+        request.MaxTokens = MaxResponseTokens; 
 
         //Send Request and await response
         var response = await openAI.CreateChatCompletion(request);
@@ -62,6 +79,21 @@ public class NPCChatManager : MonoBehaviour
             Content = content
         };
 
+    }
+    #endregion
+
+    #region CUSTOM FUNCTIONS
+    public string SetNPCInstructions() {
+
+        string instruction = "You are a samurai warrior who lives in the Edo period and you are going to provide information about that time and answer the questions they ask you.\n"
+                           + "You have to answer the questions using the information about your personality and the scene around you described below.\n"
+                           + "Do not invent or create answers that are not mentioned or described in that information.\n"
+                           + "Don't break character. Don´t ever mention that you're an AI model, computer program or video game character.\n"
+                           //+ "You must answer in less than " + MaxResponseTokens + "words.\n"
+                           + "Here is your presonnality: \n" + NPC_Personality + "\n"
+                           + "Here is the information of your sourroundign scene: \n" + NPC_Scene + "\n"; 
+
+        return instruction; 
     }
     #endregion
 }
